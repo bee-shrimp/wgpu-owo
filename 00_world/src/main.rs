77eb947560e758dfd878943f34b142eb20d3e56f
beeshrimp <cuiniao_nur.rs@proton.me>
2026-08-05 14:52:45 +0900

    added dt and need_redraw

--- 00_world/src/main.rs
+++ 00_world/src/main.rs
@@ -3,6 +3,7 @@
 
 use std::collections::HashSet;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 
 use winit::{
     application::ApplicationHandler,
@@ -18,23 +19,28 @@ use renderer::Renderer;
 mod world;
 use world::{Direction, Rect};
 
-// const TARGET_FPS: f64 = 30.0;
-// const FRAME_TIME: f64 = 1.0 / TARGET_FPS;
+const TARGET_FPS: f64 = 60.0;
+const FRAME_TIME: f64 = 1.0 / TARGET_FPS;
+const EPSILON: f32 = 0.001;
 
 #[derive(Default)]
 struct App {
     renderer: Option<Renderer>,
     pressed_keys: HashSet<KeyCode>,
     rect: Option<Rect>,
-    // last_frame_time: Instant,
-    // next_frame_time: Instant,
+    last_frame_time: Option<Instant>,
+    next_frame_time: Option<Instant>,
+    last_draw_data: (f32, f32),
+    need_redraw: bool,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        // let time = Instant::now();
-        // self.last_frame_time: time,
-        // self.next_frame_time: time + Duration::from_secs_f64(FRAME_TIME),
+        let time = Instant::now();
+        self.last_frame_time = Some(time);
+        self.next_frame_time = Some(time + Duration::from_secs_f64(FRAME_TIME));
+        self.last_draw_data = (0.0, 0.0);
+        self.need_redraw = true;
         // --------------------------------------------------------------------------- create window object
         let window = Arc::new(
             event_loop
@@ -67,13 +73,17 @@ impl ApplicationHandler for App {
             WindowEvent::Resized(size) => renderer.resize(size.width, size.height),
 
             // ----------------------------------------------------------------------- redraw
-            WindowEvent::RedrawRequested => match renderer.render() {
-                Ok(_) => {}
-                Err(e) => {
-                    log::error!("{e}");
-                    event_loop.exit();
+            WindowEvent::RedrawRequested => {
+                if self.need_redraw {
+                    match renderer.render() {
+                        Ok(_) => {}
+                        Err(e) => {
+                            log::error!("{e}");
+                            event_loop.exit();
+                        }
+                    }
                 }
-            },
+            }
 
             // ----------------------------------------------------------------------- handle key inputs
             WindowEvent::KeyboardInput {
@@ -100,45 +110,62 @@ impl ApplicationHandler for App {
     // ------------------------------------------------------------------------------- things to do after everyting else
     #[allow(unused_variables)]
     fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
-        // let now = Instant::now();
-        // let dt = now.duration_since(self.last_frame_time).as_secs_f32();
-        // let gap = 1.0 / dt;
+        let now = Instant::now();
+        let last = self.last_frame_time.unwrap();
+        let mut next = self.next_frame_time.unwrap();
+
+        let dt = now.duration_since(last).as_secs_f32();
+
+        let gap = 1.0 / dt; //fps
         // println!("{:.2}", gap);
 
-        // self.next_frame_time += Duration::from_secs_f64(FRAME_TIME);
-        // if now < self.next_frame_time {
-        //     std::thread::sleep(self.next_frame_time - now);
-        // } else {
-        //     self.next_frame_time = now
-        // }
-        // self.last_frame_time = now;
-
-        let renderer = self.renderer.as_mut().unwrap();
-        let rect = self.rect.as_mut().unwrap();
-        let mut direction_x: Direction = Direction::Still;
-        let mut direction_y: Direction = Direction::Still;
+        next += Duration::from_secs_f64(FRAME_TIME);
+        if now < next {
+            std::thread::sleep(next - now);
+        } else {
+            self.next_frame_time = Some(now)
+        }
+        self.last_frame_time = Some(now);
 
         let keys = &self.pressed_keys;
+        if keys.is_empty() {
+            self.need_redraw = false
+        } else {
+            let renderer = self.renderer.as_mut().unwrap();
+            let rect = self.rect.as_mut().unwrap();
+            let mut direction_x: Direction = Direction::Still;
+            let mut direction_y: Direction = Direction::Still;
 
-        if keys.contains(&KeyCode::ArrowUp) {
-            direction_y = Direction::Up
-        }
+            if keys.contains(&KeyCode::ArrowUp) {
+                direction_y = Direction::Up
+            }
 
-        if keys.contains(&KeyCode::ArrowLeft) {
-            direction_x = Direction::Left
-        }
-        if keys.contains(&KeyCode::ArrowDown) {
-            direction_y = Direction::Down
-        }
+            if keys.contains(&KeyCode::ArrowLeft) {
+                direction_x = Direction::Left
+            }
+            if keys.contains(&KeyCode::ArrowDown) {
+                direction_y = Direction::Down
+            }
 
-        if keys.contains(&KeyCode::ArrowRight) {
-            direction_x = Direction::Right
-        }
+            if keys.contains(&KeyCode::ArrowRight) {
+                direction_x = Direction::Right
+            }
 
-        let direction = (direction_x, direction_y);
-        let rect_pos = rect.update(direction);
-        renderer.update(rect_pos);
-        renderer.get_window().request_redraw()
+            let direction = (direction_x, direction_y);
+            let rect_pos = rect.update(dt, direction);
+
+            let x_dist = self.last_draw_data.0 - rect_pos.0;
+            let y_dist = self.last_draw_data.1 - rect_pos.1;
+
+            if x_dist.abs() >= EPSILON || y_dist.abs() >= EPSILON {
+                self.need_redraw = true;
+                self.last_draw_data = rect_pos;
+                renderer.update(rect_pos);
+                renderer.get_window().request_redraw()
+            } else {
+                self.need_redraw = false;
+            }
+        }
     }
 }
 
